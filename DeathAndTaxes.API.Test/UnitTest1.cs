e565d79c0bf66d88b6f1b9334ec019e7f408e8f4
Werner <AKA.Werner@Gmail.com>
2023-11-07 11:42:47 +0200

    Add Test sol
    Add test solution for TDD design to develop from.

---
+++ DeathAndTaxes.API.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DeathAndTaxes.API.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
