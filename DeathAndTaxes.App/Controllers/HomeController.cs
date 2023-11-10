071519f70b6a03947988d398fa1c1f6a2658cc56
Werner <AKA.Werner@Gmail.com>
2023-11-10 14:33:15 +0200

    App and APi working together
    The app and the api working together and the value is stored to the db.

--- DeathAndTaxes.App/Controllers/HomeController.cs
+++ DeathAndTaxes.App/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using DeathAndTaxes.App.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace DeathAndTaxes.App.Controllers
@@ -7,14 +8,56 @@ namespace DeathAndTaxes.App.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(string postcode, string income)
+        {
+            
+            using (var client = new HttpClient())
+            {
+                var baseUrl = _config.GetValue<string>("ApiUrl"); // "appsettings inected"
+                var apiUrl = baseUrl+"TaxCalculator/CalculateAndStoreTax";
+                var request = new HttpRequestMessage(HttpMethod.Post, $"{apiUrl}?postcode={postcode}&income={income}");
+                var content = new StringContent("", encoding: null, mediaType: "text/plain");
+                request.Content = content;
+
+                try
+                {                    
+                    var response = await client.SendAsync(request);                    
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        // Handle non-success status codes
+                        var erormessage = $"Error: {response.StatusCode} - {await response.Content.ReadAsStringAsync()}";
+                        ViewBag.Tax = erormessage;
+                        Console.WriteLine(erormessage);
+                    }
+                    else
+                    {
+                        ViewBag.Tax = await response.Content.ReadAsStringAsync();
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    var erormessage = $"An error occurred: {ex.Message}";
+                    // Handle exception
+                    Console.WriteLine(erormessage);
+                    ViewBag.Tax = erormessage;
+                }
+
+
+            }            
             return View();
         }
 
@@ -28,5 +71,7 @@ namespace DeathAndTaxes.App.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
